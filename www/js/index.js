a398c1bd1f7c9044d7abb25dbfb4c27661e74231
isma91 <ismaydogmus@gmail.com>
2016-03-18 17:58:11 +0100

    on verifie si les parties importantes sont remplis ou pas

--- www/js/index.js
+++ www/js/index.js
@@ -2,9 +2,15 @@
 /*jslint devel : true*/
 /*global $, jQuery*/
 $(document).ready(function () {
-	var user_nom, user_prenom, user_email, user_tel, parrain_email, parrain_tel, don;
+	var user_nom, user_prenom, user_email, user_adresse, user_tel, parrain_email, parrain_tel, don, error_message, error_count;
+    error_message = "";
+    error_count = 0;
+    /* ces deux localStorage seront utiliser pour savoir si l'utilisateur est déjà connecter ou pas */
     console.log(localStorage.etoile_de_martin_user);
     console.log(localStorage.etoile_de_martin_pass);
+    function change_to_invalide (id_selector) {
+        $('#' + id_selector).css('border-bottom', '1px solid #FF0000');
+    }
     $('select').material_select();
     $('#montant_don').change(function () {
     	if ($(this).val() === "autre") {
@@ -14,7 +20,8 @@ $(document).ready(function () {
     $('#valider_don').click(function () {
     	user_nom = $('#user_nom').val();
     	user_prenom = $('#user_prenom').val();
-    	user_email = $('#user_email').val();
+        user_email = $('#user_email').val();
+    	user_adresse = $('#user_adresse').val();
     	user_tel = $('#user_tel').val();
     	parrain_email = $('#parrain_email').val();
     	parrain_tel = $('#parrain_tel').val();
@@ -23,6 +30,39 @@ $(document).ready(function () {
     	} else {
     		don = $('#don').val();
     	}
-    	console.log(don);
+        if ($.trim(user_nom) === "") {
+            change_to_invalide('user_nom');
+            error_message = error_message + '<p>Vous devez ecrire votre nom</p>';
+            error_count = error_count + 1;
+        }
+        if ($.trim(user_prenom) === "") {
+            change_to_invalide('user_prenom');
+            error_message = error_message + '<p>Vous devez ecrire votre prenom</p>';
+            error_count = error_count + 1;
+        }
+        if ($.trim(user_email) === "") {
+            change_to_invalide('user_email');
+            error_message = error_message + '<p>Vous devez ecrire votre email</p>';
+            error_count = error_count + 1;
+        }
+        if ($.trim(user_adresse) === "") {
+            change_to_invalide('user_adresse');
+            error_message = error_message + '<p>Vous devez ecrire votre adresse</p>';
+            error_count = error_count + 1;
+        }
+        if ($.trim(don) < 1 || $.trim(don) === "autre") {
+            change_to_invalide('don');
+            error_message = error_message + '<p>Vous devez ecrire le montant de votre don</p>';
+            error_count = error_count + 1;            
+        }
+        if (error_count !== 0) {
+            $(this).css('background-color', '#FF0000');
+            $('#div_error').html(error_message);
+            $(this).css('background-color', '#2ab7a9');
+            error_message = "";
+            error_count = 0;
+        } else {
+            /* faire la requete ajax pour envoyer le don */
+        }
     });
 });
