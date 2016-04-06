54cba92d7e46823941a503993c757ada9c529adc
isma91 <ismaydogmus@gmail.com>
2016-04-06 10:56:15 +0200

    mise a jour du model pour le mettre comme la base de donnee

--- api/model/User.php
+++ api/model/User.php
@@ -4,21 +4,55 @@ namespace model;
 use model\Model;
 class User extends Model
 {
-	private $_firstname;
-	private $_lastname;
-	private $_email;
-	private $_telephone;
-	private $_amount;
+    private $_nom;
+    private $_prenom;
+    private $_email;
+    private $_pass;
+    private $_tel;
+    private $_parrainEmail;
+    private $_parrainTel;
+    private $_newsletter;
 
-	public function setFirstname($firstname) {$this->_firstname = $firstname;}
-	public function setLastname($lastname) {$this->_lastname = $lastname;}
-	public function setEmail($email) {$this->_email = $email;}
-	public function setTelephone($telephone) {$this->_telephone = $telephone;}
-	public function setAmount($amount) {$this->_amount = $amount;}
-
-	public function getFirstname() {return $this->_firstname;}
-	public function getLastname() {return $this->_lastname;}
-	public function getEmail() {return $this->_email;}
-	public function getTelephone() {return $this->_telephone;}
-	public function getAmount() {return $this->_amount;}
+    public function setNom ($nom) {
+        $this->_nom = $nom;
+    }
+    public function setPrenom ($prenom) {
+        $this->_prenom = $prenom;
+    }
+    public function setEmail ($email) {
+        $this->_email = $email;
+    }
+    public function setPass ($pass) {
+        $this->_pass = $pass;
+    }
+    public function setTel ($tel) {
+        $this->_tel = $tel;
+    }
+    public function setParrainEmail ($parrainEmail) {
+        $this->_parrainEmail = $parrainEmail;
+    }
+    public function setParrainTel ($parrainTel) {
+        $this->_parrainTel = $parrainTel;
+    }
+    public function getNom() {
+        return $this->_nom;
+    }
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+    public function getEmail() {
+        return $this->_email;
+    }
+    public function getPass() {
+        return $this->_pass;
+    }
+    public function getTel() {
+        return $this->_tel;
+    }
+    public function getParrainEmail() {
+        return $this->_parrainEmail;
+    }
+    public function getParrainTel() {
+        return $this->_parrainTel;
+    }
 }
