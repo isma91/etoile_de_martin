3986da28a025caaaec0bb03300591521b3ce06fe
isma91 <ismaydogmus@gmail.com>
2016-04-07 16:51:31 +0200

    ajout de la function connexion

--- api/public/index.php
+++ api/public/index.php
@@ -7,17 +7,15 @@ $users_controller = new UsersController();
 
 switch ($_POST["action"]) {
 	case 'check_parrain':
-	$users_controller->check_parrain($_POST["parrain_email"], $_POST["parrain_tel"]);
+	$users_controller->check_parrain($_POST["parrain_email"]);
 	break;
 	case 'user_inscription':
 	$users_controller->create_user($_POST["user_nom"], $_POST["user_prenom"], $_POST["user_adress"], $_POST["user_email"], $_POST["user_pass"], $_POST["user_tel"], $_POST["user_newsletter"]);
+	break;
+	case 'connexion':
+	$users_controller->connexion($_POST["user_email"], $_POST["user_pass"]);
 	break;
 	default:
 	echo json_encode(array('error' => "action non valide !!", 'data' => null));
 	break;
 }
-/*if ($_POST && $_GET) {
-	$o = new ApiController($_POST, $_GET);
-
-	echo $o->result();
-}*/
