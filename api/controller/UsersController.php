ac3e5b3694993955a03492d4c1f9a582cb5691be
Raph <rbleuzet@gmail.com>
2016-04-27 15:58:28 +0200

    update userscontroller

--- api/controller/UsersController.php
+++ api/controller/UsersController.php
@@ -10,89 +10,92 @@ class UsersController extends User
 		echo json_encode(array('error' => $error, 'data' => $data));
 	}
 
-	public function create_user ($nom, $prenom, $adresse, $ville, $code_postal, $pays, $email, $pass, $tel, $newsletter) {
-		if (empty($tel)) {
-			$tel = null;
-		}
-		$duplicate_email = false;
+	private function checkEmail()
+	{
 		$bdd = new Bdd();
-		$sql = $bdd->getBdd()->prepare("SELECT email FROM users");
-		$sql->execute();
-		$donnees = $sql->fetchAll();
-		foreach ($donnees as $value) {
-			if ($email === $value['email']) {
-				$duplicate_email = true;
-			}
+
+		$check = $bdd->getBdd()->prepare('SELECT id FROM users WHERE email = :email');
+		$check->bindParam(':email', $_POST['user_email'], PDO::PARAM_STR);
+		$check->execute();
+		if (empty($check->fetch())) {
+			return true;
 		}
-		if ($duplicate_email === false) {
-			$sql_insert = $bdd->getBdd()->prepare("INSERT INTO users (nom, prenom, email, pass, tel, adresse, ville, codePostal, pays, newsletter) VALUES (:nom, :prenom, :email, :pass, :tel, :adresse, :ville, :codePostal, :pays, :newsletter)");
-			$sql_insert->bindParam(":nom", $nom);
-			$sql_insert->bindParam(":prenom", $prenom);
-			$sql_insert->bindParam(":email", $email);
-			$sql_insert->bindParam(":pass", password_hash($pass, PASSWORD_DEFAULT));
-			$sql_insert->bindParam(":tel", $tel);
-			$sql_insert->bindParam(":adresse", $adresse);
-			$sql_insert->bindParam(":ville", $ville);
-			$sql_insert->bindParam(":codePostal", $code_postal);
-			$sql_insert->bindParam(":pays", $pays);
-			$sql_insert->bindParam(":newsletter", $newsletter);
-			if ($sql_insert->execute()) {
+
+		return false;
+	}
+	public function create_user ($nom, $prenom, $adresse, $ville, $code_postal, $pays, $email, $pass, $tel, $newsletter)
+	{
+		$bdd = new Bdd();
+		if ($this->checkEmail()) {
+			$sql = 'INSERT INTO users (nom, prenom, email, pass, tel, adresse, ville, codePostal, pays, newsletter) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+			$create = $bdd->getBdd()->prepare($sql);
+			$create->bindParam(1, $nom);
+			$create->bindParam(2, $prenom);
+			$create->bindParam(3, $email);
+			$create->bindParam(4, password_hash($pass, PASSWORD_DEFAULT));
+			$create->bindParam(5, $tel);
+			$create->bindParam(6, $adresse);
+			$create->bindParam(7, $ville);
+			$create->bindParam(8, $code_postal);
+			$create->bindParam(9, $pays);
+			$create->bindParam(10, $newsletter);
+			if ($create->execute()) {
 				$token = sha1(time() * rand(1, 555));
-				$sql_token = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE email = :email');
-				$sql_token->bindParam(':token', $token);
-				$sql_token->bindParam(':email', $email);
-				if ($sql_token->execute()) {
+				$update = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE email = :email AND prenom = :prenom');
+				$update->bindParam(':token', $token);
+				$update->bindParam(':email', $email);
+				$update->bindParam(':prenom', $prenom);
+				if ($update->execute()) {
 					self::send_json(null, array("email" => $email, "token" => $token));
+					return true;
 				} else {
 					self::send_json("Inscription reussi mais probleme d'ajout de token !! Contacter l'admin du logiciel !!", null);
+					return false;
 				}
 			} else {
 				self::send_json("Probleme lors de l'inscription", null);
+				return false;
 			}
-		} else {
-			self::send_json("L'email " . $email . " est déjà pris !! Veuillez en ecrire un autre !!", null);
 		}
+		self::send_json("L'email " . $email . " est déjà pris !! Veuillez en ecrire un autre !!", null);
+		return false;
 	}
 	public function check_parrain ($email) {
 		$bdd = new Bdd();
-		$sql = $bdd->getBdd()->prepare("SELECT email FROM users WHERE email = ?");
+		$sql = $bdd->getBdd()->prepare('SELECT email FROM users WHERE email = ?');
 		$sql->bindParam(1, $email);
 		$sql->execute();
-		$donnees = $sql->fetch();
-		if ($donnees !== false) {
+		$donnees = $sql->fetch(PDO::FETCH_ASSOC);
+		if ($donnees) {
 			self::send_json(null, $donnees["email"]);
 		} else {
 			self::send_json("L'email " . $email . " est associé avec aucun inscris !!", null);
 		}
 	}
+
 	public function connexion($email, $pass) {
 		$bdd = new Bdd();
-		$sql_email = $bdd->getBdd()->prepare("SELECT email FROM users WHERE email = :email");
-		$sql_email->bindParam(":email", $email);
-		$sql_email->execute();
-		$donnee_email = $sql_email->fetch();
-		if ($donnee_email !== false) {
-			$sql_pass = $bdd->getBdd()->prepare("SELECT pass FROM users WHERE email = :email");
-			$sql_pass->bindParam(":email", $email);
-			$sql_pass->execute();
-			$donnee_pass = $sql_pass->fetch();
-			$check_pass = password_verify($pass, $donnee_pass["pass"]);
-			if ($check_pass) {
+		$auth_error = true;
+		$getPwd = $bdd->getBdd()->prepare('SELECT pass FROM users WHERE email = ?');
+		$getPwd->bindParam(1, $email);
+		$getPwd->execute();
+		if ($pwd = $getPwd->fetch(PDO::FETCH_ASSOC)) {
+			if (password_verify($pass, $pwd['pass'])) {
+				$auth_error = false;
 				$token = sha1(time() * rand(1, 555));
-				$sql_token = $bdd->getBdd()->prepare('UPDATE users SET token = :token WHERE email = :email');
-				$sql_token->bindParam(':token', $token);
-				$sql_token->bindParam(':email', $email);
-				if ($sql_token->execute()) {
-					self::send_json(null, array("email" => $email, "token" => $token));
-				} else {
-					self::send_json("Bon email et pass mais probleme d'ajout de token !! Contacter l'admin du logiciel !!", null);
+				$sql_token = $bdd->getBdd()->prepare('UPDATE users SET token = ? WHERE email = ?');
+				$sql_token->bindParam(1, $token, PDO::PARAM_STR);
+				$sql_token->bindParam(2, $email, PDO::PARAM_STR);
+				if (!$sql_token->execute()) {
+					self::send_json("error occured during token insertion, please contact admin", null);
+					return false;
 				}
-			} else {
-				self::send_json("Mauvais email ou mot de pass !!", null);
+				self::send_json(false, array("email" => $email, "token" => $token));
+				return true;
 			}
-		} else {
-			self::send_json("Mauvais email ou mot de pass !!", null);
 		}
+		self::send_json("Email ou mot de passe incorrect", null);
+		return false;
 	}
 
 	public function confirm()
@@ -114,8 +117,12 @@ class UsersController extends User
 			if ($mail->sendMail()) {
 				$this->send_json(false, null);
 				return true;
+			} else {
+				$this->send_json('error occured during mail sending, please contact admin', null);
+				return false;
 			}
 		}
+		$this->send_json('error occcured during base insertion until confirm', null);
 		return false;
 	}
 }
